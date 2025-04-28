0de629e5d6ff33b794b3c2a6cc40f54ce692bbe6
Varpenikhil0007 <varpenikhil0007@gmail.com>
2025-04-28 21:42:12 +0530

    first commit

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ const app = express();
 
 // Middleware
 app.use(cors({
-    origin: 'https://upi-fraud-detection-seven.vercel.app/',
+    origin: 'https://upi-fraud-detection-seven.vercel.app',
     credentials: true
 }));
 app.use(express.json());
